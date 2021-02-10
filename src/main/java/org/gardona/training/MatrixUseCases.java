96e2ff7c14233e4b5a020f00cde4d4e75e22689b
Júlio César de Brito Gardona <jcbritobr@gmail.com>
2021-02-10 11:31:30 -0300

    Add matrix use cases example.

---
+++ src/main/java/org/gardona/training/MatrixUseCases.java
@@ -0,0 +1,16 @@
+package org.gardona.training;
+
+import org.opencv.core.Core;
+
+public class MatrixUseCases {
+	
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
