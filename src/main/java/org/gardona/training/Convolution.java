6c7bf29143da8d2e6b611e1418459b976565e43a
Júlio César de Brito Gardona <jcbritobr@gmail.com>
2021-02-17 11:29:52 -0300

    Add new camera capture example.

---
+++ src/main/java/org/gardona/training/Convolution.java
@@ -0,0 +1,2 @@
+package org.gardona.training;public class Convolution {
+}
--- src/main/java/org/gardona/training/MatrixUseCases.java
+++ src/main/java/org/gardona/training/MatrixUseCases.java
@@ -1,65 +1,40 @@
 package org.gardona.training;
 
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Range;
-import org.opencv.core.Scalar;
-import org.opencv.highgui.HighGui;
+import org.opencv.core.*;
 import org.opencv.imgcodecs.Imgcodecs;
 
 public class MatrixUseCases {
 
-	private static final String RESULT_IMAGE = "Result image";
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
 
-	static {
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-	}
+    public static void main(String[] args) {
+        // creates an matrix that's describe an image with 1 channel 100 level gray
+        Mat image1 = new Mat(2, 3, CvType.CV_8U, new Scalar(100));
+        System.out.println(image1.elemSize());
+        System.out.println(image1);
+        System.out.println(image1.dump());
+        // creates an matrix that's describe a 3byte/p color magenta - rgb(255, 0, 255)
+        Mat image2 = new Mat(3, 3, CvType.CV_8UC3);
+        // set image 2 to magenta color
+        image2.setTo(new Scalar(255, 0, 255));
+        System.out.println(image2.dump());
 
-	public static void main(String[] args) {
-		// creates an matrix thats describe an image with 1 channel 100 level gray
-		Mat image1 = new Mat(240, 320, CvType.CV_8U, new Scalar(100));
-		HighGui.namedWindow(RESULT_IMAGE, HighGui.WINDOW_AUTOSIZE);
-		showMatrix(image1);
-		HighGui.waitKey();
+        Mat image3 = Mat.eye(3, 3, CvType.CV_8U);
+        System.out.println(image3.dump());
 
-		// creates an matrix thats describe a 3byte/p color magenta - rgb(255, 0, 255)
-		Mat image2 = new Mat(240, 320, CvType.CV_8UC3);
-		// set image 2 to magenta color
-		image2.setTo(new Scalar(255, 0, 255));
-		showMatrix(image2);
-		HighGui.waitKey();
+        Mat zeroes = Mat.zeros(3, 3, CvType.CV_8U);
+        System.out.println(zeroes);
 
-		Mat image3 = Imgcodecs.imread("images/flower.jpeg");
-		showMatrix(image3);
-		HighGui.waitKey();
+        Mat image4 = Mat.ones(3, 3, CvType.CV_8U);
+        System.out.println(image4.dump());
 
-		// creates a new matrix with another coodinates
-		Mat image4 = new Mat(image3, new Range(0, 200), new Range(0, 200));
-		showMatrix(image4);
-		HighGui.waitKey();
+        var flower = Imgcodecs.imread("images/flower.jpeg");
 
-		// Clone a matrix
-		Mat image5 = image4.clone();
-		showMatrix(image5);
-		HighGui.waitKey();
+        // Submat the flower matrix. This operation takes a subsection of original matrix
+        var submat = flower.submat(new Rect(200, 200, 500, 400));
+        System.out.println(submat);
 
-		// horizontally flip image5
-		Core.flip(image5, image5, 1);
-		showMatrix(image5);
-		HighGui.waitKey();
-
-		// vertically flip image5
-		Core.flip(image5, image5, 0);
-		showMatrix(image5);
-		HighGui.waitKey();
-
-		System.exit(0);
-
-	}
-
-	private static void showMatrix(Mat image) {
-		HighGui.imshow(RESULT_IMAGE, image);
-	}
-
+    }
 }
